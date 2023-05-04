219d79cf059f725f66e040f96c5734dac9520b5a
Radosław Rzepka <radoslawrzepka02@gmail.com>
2023-05-04 10:47:26 +0200

    configuration of project

--- src/services/config/firebaseConfig.js
+++ src/services/config/firebaseConfig.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
 	apiKey: import.meta.env.apiKey,
 	authDomain: import.meta.env.authDomain,
@@ -15,5 +10,4 @@ const firebaseConfig = {
 	measurementId: import.meta.env.measurementId,
 };
 
-// Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+export const firebaseApp = initializeApp(firebaseConfig);
