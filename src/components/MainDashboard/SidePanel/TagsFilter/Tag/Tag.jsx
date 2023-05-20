bb8a8c70382236b5c642c93571cebda2485e045b
Radosław Rzepka <radoslawrzepka02@gmail.com>
2023-05-20 03:21:23 +0200

    fixing styling

--- src/components/MainDashboard/SidePanel/TagsFilter/Tag/Tag.jsx
+++ src/components/MainDashboard/SidePanel/TagsFilter/Tag/Tag.jsx
@@ -7,7 +7,7 @@ const Tag = props => {
 	return (
 		<div
 			className={`m-1 h-max w-max cursor-pointer rounded-xl border px-4 py-1 text-xs ${
-				props.isChosen
+				!props.isChosen
 					? "border-gray-50 bg-gray-50 text-gray-700"
 					: "border-gray-700 bg-gray-700 text-gray-50"
 			}`}
